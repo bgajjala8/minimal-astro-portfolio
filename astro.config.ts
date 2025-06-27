33ce166f30266b61dc2511835039e5d0ae833dc9
Bharath Gajjala <120367134+bgajjala8@users.noreply.github.com>
2025-06-27 16:38:35 -0500

    Oops

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  output: "static",
-  prefetch: true,
-  compressHTML: true,
   site: 'https://bgajjala8.github.io',
 });
