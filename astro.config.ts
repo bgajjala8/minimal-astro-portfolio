4e2537353b33c8768e29c2b8f08cf90e19fce903
Bharath Gajjala <120367134+bgajjala8@users.noreply.github.com>
2025-06-28 00:29:20 -0500

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import relativeLinks from 'astro-relative-links';
 
 export default defineConfig({
   site: 'https://bgajjala8.github.io',
   base: '/minimal-astro-portfolio/',
   output: "static",
-  integrations: [relativeLinks()],
 });
