1e9e738977acef66e46ead4afd05c61542552b6b
Bharath Gajjala <120367134+bgajjala8@users.noreply.github.com>
2025-06-28 00:21:56 -0500

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://bgajjala8.github.io',
-  base: '/minimal-astro-portfolio',
+  base: '/minimal-astro-portfolio/',
   output: "static",
   prefetch: true,
   compressHTML: true,
