8d84b96f6a1cea5559985941bc098026dc8b9bf2
Bharath Gajjala <120367134+bgajjala8@users.noreply.github.com>
2025-06-30 12:55:54 -0500

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,40 +1,9 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import remarkToc from "remark-toc";
-import remarkCollapse from "remark-collapse";
-import sitemap from "@astrojs/sitemap";
-import { SITE } from "./src/config";
+import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  site: SITE.website,
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    sitemap(),
-  ],
-  markdown: {
-    remarkPlugins: [
-      remarkToc,
-      [
-        remarkCollapse,
-        {
-          test: "Table of contents",
-        },
-      ],
-    ],
-    shikiConfig: {
-      theme: "one-dark-pro",
-      wrap: true,
-    },
-  },
-  vite: {
-    optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
-  },
-  scopedStyleStrategy: "where",
+  site: 'https://bgajjala8.github.io',
+  base: '/minimal-astro-portfolio/',
+  output: "static",
+  prefetch: true,
+  compressHTML: true,
 });
